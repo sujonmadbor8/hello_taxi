8da50e93cb3104a45fd1bffbd761667ee57b8dfc
Sujon Madbor <sujonmadbor8@gmail.com>
2023-07-12 12:31:27 +0600

    added

--- README.txt
+++ README.txt
@@ -1,4 +1,13 @@
-# Hello-Taxi
+# bootstrap_template
 
-https://sujonmadbor8.github.io/hello_taxi/
+Hello Taxi - html css js jq
 
+
+font --------
+p --- open sans  700
+Fira Sans Condensed --500
+
+color ---------
+yellow #ffc61a
+black #1F1F1F;
+white : #ffffff
--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-let menu = document.querySelector("#navbar");
-let nav = document.querySelector(".menu");
+// javScript code here
 
-let showcontactform = document.querySelector(".contact");
-let myform = document.querySelector(".contact-form");
-
-let formclose = document.querySelector("#form-close");
-
-menu.onclick = () => {
-  menu.classList.toggle("fa-times");
-  nav.classList.toggle("active");
-};
-showcontactform.onclick = () => {
-  myform.classList.toggle("active");
-};
-formclose.onclick = () => {
-  myform.classList.remove("active");
-};
+// jQuery code here
