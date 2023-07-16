59e02b499f6e1a8e1b583034cb5f1e8b0fe8e920
Sujon Madbor <sujonmadbor8@gmail.com>
2023-07-16 18:56:52 +0600

    added

--- js/script.js
+++ js/script.js
@@ -9,26 +9,24 @@ AOS.init({
 $(function () {
   // slick slider start
   $(".s_slider").slick({
-    infinite: true,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-  });
-  $(".testimonial_s").slick({
-    infinite: true,
-    slidesToShow: 3,
     slidesToScroll: 1,
     // autoplay: true,
     autoplaySpeed: 3000,
+    arrows: false,
+  });
+  $(".testimonial_s").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-          infinite: true,
-          dots: true,
         },
       },
       {
